5ead948d34233883692eed9704131c2af4b6171c
Tim Welch <tim.j.welch@gmail.com>
2022-07-08 17:29:01 -0700

    clipToOcean: drop some unit tests no example for

--- src/functions/clipToOceanEezUnit.test.ts
+++ src/functions/clipToOceanEezUnit.test.ts
@@ -63,65 +63,6 @@ describe("Basic unit tests", () => {
     }
   }, 10000);
 
-  test("clipOutsideBarbadosEez", async () => {
-    const examples = await getExampleFeaturesByName("gp-clip-ocean");
-    const example = examples[
-      "gp-clip-ocean-both-sides-barbados.json"
-    ] as Feature<Polygon>;
-    try {
-      const result = await clipOutsideEez(example, ["Barbados"]);
-      if (!result) fail("result should not be null");
-      expect(result).toBeTruthy();
-      expect(booleanValid(result));
-      expect(
-        result.geometry.type === "Polygon" ||
-          result.geometry.type === "MultiPolygon"
-      );
-      writeResultOutput(
-        result,
-        "clipOutsideBarbadosEez",
-        example.properties?.name
-      );
-    } catch (e) {
-      if (e instanceof ValidationError) {
-        // ValidationErrors don't indicate failures, just comprehensive tests
-      } else {
-        throw e;
-      }
-    }
-  }, 10000);
-
-  test("clipOutsideMultipleEez", async () => {
-    const examples = await getExampleFeaturesByName("gp-clip-ocean");
-    const example = examples[
-      "gp-clip-ocean-both-sides-barbados.json"
-    ] as Feature<Polygon>;
-    try {
-      const result = await clipOutsideEez(example, [
-        "Barbados",
-        "Trinidad and Tobago",
-      ]);
-      if (!result) fail("result should not be null");
-      expect(result).toBeTruthy();
-      expect(booleanValid(result));
-      expect(
-        result.geometry.type === "Polygon" ||
-          result.geometry.type === "MultiPolygon"
-      );
-      writeResultOutput(
-        result,
-        "clipOutsideMultipleEez",
-        example.properties?.name
-      );
-    } catch (e) {
-      if (e instanceof ValidationError) {
-        // ValidationErrors don't indicate failures, just comprehensive tests
-      } else {
-        throw e;
-      }
-    }
-  }, 10000);
-
   test("clipToOceanEez - should throw ValidationError if self-crossing", async () => {
     expect(
       async () => await clipToOceanEez(selfCrossingSketchPolygon)
